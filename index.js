aaf2b7503c79ecf04737ea05bbf823a67b97edbd
bromo-dark <79233147+bromo-dark@users.noreply.github.com>
2021-02-20 22:29:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["491725885117@s.whatsapp.net","491725885117@s.whatsapp.net"] 
 /************************************/
 
        
